93d072b49980127e7ff31b7b10914d08c274ccb9
sahilrm794 <smustilwar@gmail.com>
2025-12-23 01:41:30 +0530

    feat: add task routes for creating, updating, and deleting tasks

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import { inngest, functions } from "./inngest/index.js"
 import workspaceRouter from './routes/workspaceRoutes.js';
 import { protect } from './middlewares/authMiddleware.js';
 import projectRouter from './routes/projectRoutes.js';
+import taskRouter from './routes/taskRoutes.js';
 
 const app=express();
 
@@ -21,6 +22,7 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 //Routes
 app.use("/api/workspaces", protect, workspaceRouter)
 app.use("/api/projects",protect,projectRouter)
+app.use("/api/tasks",protect,taskRouter)
 
 const PORT = process.env.PORT || 5000
 
