087b6ee8a872b1447fa57b42931ee750907008a3
sahilrm794 <smustilwar@gmail.com>
2025-12-22 23:01:01 +0530

    fix: enhance error messages in auth middleware for clarity

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -3,11 +3,11 @@ export const protect = async (req, res, next) => {
     try {
         const { userId } = await req.auth();
         if(!userId){
-            return res.status(401).json({message: "Unauthorized"})
+            return res.status(401).json({message: "Unauthorized the user is not logged in."})
         }
         return next()
     } catch (error) {
         console.log(error);
-        res.status(401).json({ message: error.code || error.message});
+        res.status(401).json({ message: error.code || error.message + "The error occured in middleware auth"});
     }
 }
