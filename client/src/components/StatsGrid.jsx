688d6d4081be8320b05b98e8f56425b80728ed41
sahilrm794 <smustilwar@gmail.com>
2026-02-07 10:46:53 +0530

    Resolved all errors

--- client/src/components/StatsGrid.jsx
+++ client/src/components/StatsGrid.jsx
@@ -1,8 +1,10 @@
 import { FolderOpen, CheckCircle, Users, AlertTriangle } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { useUser } from "@clerk/clerk-react";
 
 export default function StatsGrid() {
+    const { user } = useUser();
     const currentWorkspace = useSelector(
         (state) => state?.workspace?.currentWorkspace || null
     );
@@ -64,7 +66,7 @@ export default function StatsGrid() {
                     (acc, project) =>
                         acc +
                         project.tasks.filter(
-                            (t) => t.assignee?.email === currentWorkspace.owner.email
+                            (t) => t.assigneeId === user?.id
                         ).length,
                     0
                 ),
